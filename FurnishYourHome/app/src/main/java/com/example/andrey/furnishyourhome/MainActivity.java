04e43efc3166ea7c6c7cf22dd18b1c5c02d2aa9d
mostcho <sspasov92@gmail.com>
2015-02-09 20:08:45 +0200

    add button

--- FurnishYourHome/app/src/main/java/com/example/andrey/furnishyourhome/MainActivity.java
+++ FurnishYourHome/app/src/main/java/com/example/andrey/furnishyourhome/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View v) {
+        Toast.makeText(this, "BUTTON", Toast.LENGTH_LONG).show();
     }
 
 
