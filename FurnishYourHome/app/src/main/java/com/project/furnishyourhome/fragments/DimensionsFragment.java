aa6c2d8448b553ff76ae7bfe4054b2ee80ba4bd0
mostcho <sspasov92@gmail.com>
2015-02-14 15:36:08 +0200

    added new activity for new room added material desig add button

--- FurnishYourHome/app/src/main/java/com/project/furnishyourhome/fragments/DimensionsFragment.java
+++ FurnishYourHome/app/src/main/java/com/project/furnishyourhome/fragments/DimensionsFragment.java
@@ -1,4 +1,5 @@
 package com.project.furnishyourhome.fragments;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,12 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.project.furnishyourhome.AddRoomActivity;
 import com.project.furnishyourhome.MainActivity;
 import com.project.furnishyourhome.R;
 
@@ -78,6 +81,15 @@ public class DimensionsFragment extends Fragment {
         });
         EditText etHeight = (EditText) rootView.findViewById(R.id.et_height);
         EditText etDepth = (EditText) rootView.findViewById(R.id.et_depth);
+
+        Button btn = (Button) rootView.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), AddRoomActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
